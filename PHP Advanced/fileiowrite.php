f8d7160f385a91c45a6147cac6610e659cb543b3
Ronald Nijssen <hed320@hotmail.com>
2016-03-07 15:35:32 +0100

    Fileio read/write

---
+++ PHP Advanced/fileiowrite.php
@@ -0,0 +1,8 @@
+<?php
+$filename = "files/SCORE.TXT" ;
+$filestream = fopen( $filename, "w") or die("Unable to open file!");
+$contents = "Score van het spel \n";
+$contents .= "John Doe \t 10 punten\n";
+$contents .= "Computer \t 7 punten \n";
+fwrite( $filestream, $contents );
+fclose( $filestream );
