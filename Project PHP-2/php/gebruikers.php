504c9ee597e209776807c4181939e4fd1d91ae8a
Ronald Nijssen <hed320@hotmail.com>
2016-03-10 10:14:16 +0100

    Updated gebruikers.php

--- Project PHP-2/php/gebruikers.php
+++ Project PHP-2/php/gebruikers.php
@@ -3,9 +3,38 @@
 $content = new TemplatePower("html/gebruikers.html");
 $content->prepare();
 
-$verbinding = new PDO("mysql:host=localhost;dbname=project3", "root", "");
+try {
+    $verbinding = new PDO("mysql:host=localhost;dbname=project3", "root", "");
+    $verbinding->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $error) {
+    echo "Regelnummer : ".$error->getLine()."<br>";
+    echo "Bestand : ".$error->getFile()."<br>";
+    echo "Foutmelding : ".$error->getMessage()."<br>";
+}
 
+/*
 $getusers = $verbinding->query("SELECT voornaam, achternaam, email from gebruikers");
+
+while ($gebruikers = $getusers->fetch(PDO::FETCH_ASSOC)) {
+    $content->newBlock("rij");
+    $content->assign(array(
+        "voornaam" => $gebruikers["voornaam"],
+        "achternaam" => $gebruikers["achternaam"],
+        "email" => $gebruikers["email"]
+    ));
+}
+*/
+
+try {
+    //Met externe variabel, prepare gebruiken.
+    $getusers = $verbinding->prepare("SELECT voornaam, achternaam, email FROM gebruikers WHERE idgebruikers = :id");
+    $getusers->bindParam(":id", $_GET["id"]);
+    $getusers->execute();
+} catch(PDOException $error) {
+    echo "Regelnummer : ".$error->getLine()."<br>";
+    echo "Bestand : ".$error->getFile()."<br>";
+    echo "Foutmelding : ".$error->getMessage()."<br>";
+}
 
 while ($gebruikers = $getusers->fetch(PDO::FETCH_ASSOC)) {
     $content->newBlock("rij");
