cfbb79f2dc4176a3aa60d99c39b288a3e7d4927b
Ronald Nijssen <hed320@hotmail.com>
2016-03-10 09:24:06 +0100

    Updated project3

--- Project PHP-2/php/gebruikers.php
+++ Project PHP-2/php/gebruikers.php
@@ -2,3 +2,16 @@
 
 $content = new TemplatePower("html/gebruikers.html");
 $content->prepare();
+
+$verbinding = new PDO("mysql:host=localhost;dbname=project3", "root", "");
+
+$getusers = $verbinding->query("SELECT voornaam, achternaam, email from gebruikers");
+
+while ($gebruikers = $getusers->fetch(PDO::FETCH_ASSOC)) {
+    $content->newBlock("rij");
+    $content->assign(array(
+        "voornaam" => $gebruikers["voornaam"],
+        "achternaam" => $gebruikers["achternaam"],
+        "email" => $gebruikers["email"]
+    ));
+}
