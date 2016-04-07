39eaca5bef46824255e5d07b2393aaa722a3361e
Ronald Nijssen <hed320@hotmail.com>
2016-04-07 08:44:58 +0200

    project 3

--- Project PHP-2/Lessen/php/locaties.php
+++ Project PHP-2/Lessen/php/locaties.php
@@ -15,28 +15,82 @@ $content->assign('PAGEID', $pagina['idbestanden']);
 
 switch ($actie) {
 	case 'wijzigen':
-		if (!empty($_POST['naam']) AND !empty($_POST['adres'])	AND !empty($_POST['plaats'])) {
-			$wijzigen = $verbinding->prepare("UPDATE locatie SET  WHERE idlocatie = :idlocatie");
+
+		if(!empty($_POST['naam']) AND !empty($_POST['adres']) AND !empty($_POST['plaats'])) {
+
+			/**
+			 * Stap 1:
+			 * 		Prepare|Query
+			 *
+			 *		Keuze is: Prepare
+			 *
+			 * Stap 2:
+			 * 		SQL schrijven
+			 *
+			 * 		UPDATE locatie SET ... WHERE idlocatie = :idlocatie
+			 *
+			 * Stap 3:
+			 *
+			 * 		Pdo variable binden
+			 *
+			 * Stap 4:
+			 *
+			 * 		Execute
+			 *
+			 * Stap 5:
+			 *
+			 * 		Fetch resultaat
+			 *
+			 */
+			$wijzigen = $verbinding->prepare("");
+
+
+
+		} else {
+			/**
+			 * Stap 1:
+			 * 		Prepare|Query
+			 *
+			 *		Keuze is: Prepare
+			 *
+			 * Stap 2:
+			 * 		SQL schrijven
+			 *
+			 * 		SELECT * FROM locatie WHERE idlocatie = :idlocatie
+			 *
+			 * Stap 3:
+			 *
+			 * 		Pdo variable binden
+			 *
+			 * Stap 4:
+			 *
+			 * 		Execute
+			 *
+			 * Stap 5:
+			 *
+			 * 		Fetch resultaat
+			 *
+			 */
+			$wijzigen = $verbinding->prepare("SELECT * FROM locatie WHERE idlocatie= :idlocatie");
+			$wijzigen->bindParam('idlocatie', $_GET['locatieid']);
+			$wijzigen->execute();
+			$locatie = $wijzigen->fetch(PDO::FETCH_ASSOC);
+
+			$content->newBlock('FORMULIER');
+			$content->assign('ACTION', 'wijzigen');
+			$content->assign('PAGEID', $pagina['idbestanden']);
+			$content->assign([
+				'ID' => $locatie['idlocatie'],
+				'NAAM' => $locatie['naam'],
+				'ADRES' => $locatie['adres'],
+				'PLAATS' => $locatie['plaats']
+			]);
 		}
 
-		$wijzigen = $verbinding->prepare("SELECT * FROM locatie WHERE idlocatie = :idlocatie");
-		$wijzigen->bindParam(":idlocatie", $_GET["locatieid"]);
-		$wijzigen->execute();
 
-		$locatie = $wijzigen->fetch(PDO::FETCH_ASSOC);
 
-		$content->newBlock('FORMULIER');
-		$content->assign('ACTION', "wijzigen");
-		$content->assign('PAGEID', $pagina['idbestanden']);
-		$content->assign([
-			"ID" => $locatie["idlocatie"],
-			"NAAM" => $locatie["naam"],
-			"ADRES" => $locatie["adres"],
-			"PLAATS" => $locatie["plaats"]
-		]);
 		break;
 	case 'toevoegen':
-
 		if (!isset($_POST['naam']) && !isset($_POST['adres']) && !isset($_POST['plaats'])) {
 			$content->newBlock('FORMULIER');
 			$content->assign('PAGEID', $pagina['idbestanden']);
@@ -61,17 +115,24 @@ switch ($actie) {
 		}
 		break;
 	default:
-
 		$locaties = $verbinding->query("SELECT * FROM locatie");
 		$locaties->execute();
 
 		$content->newBlock('OVERZICHT');
 		while ($locatie = $locaties->fetch(PDO::FETCH_ASSOC)) {
 			$content->newBlock('RIJ');
+			$content->assign('PAGEID', $pagina['idbestanden']);
 			$content->assign([
 				'NAAM' => $locatie['naam'],
 				'ADRES' => $locatie['adres'],
-				'PLAATS' => $locatie['plaats']
+				'PLAATS' => $locatie['plaats'],
+				'IDLOCATIE' => $locatie['idlocatie']
 			]);
 		}
+
+
+
+
+
+
 }
