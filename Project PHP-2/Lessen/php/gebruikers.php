d987648eea9a7990b85a86790e3afdbfe8714ba3
Ronald Nijssen <hed320@hotmail.com>
2016-04-07 08:44:58 +0200

    project 3

--- Project PHP-2/Lessen/php/gebruikers.php
+++ Project PHP-2/Lessen/php/gebruikers.php
@@ -15,7 +15,8 @@ $content->assign('PAGEID', $pagina['idbestanden']);
 
 switch ($actie) {
 	case "toevoegen":
-		if (!empty($_POST['voornaam']) AND !empty($_POST['achternaam'])	AND !empty($_POST['email'])
+		if (!empty($_POST['voornaam']) AND !empty($_POST['achternaam'])
+			AND !empty($_POST['email'])
 		) {
 			// form moet gepost zijn
 			try {
@@ -33,6 +34,7 @@ switch ($actie) {
 				$content->newBlock("ERROR");
 				$content->assign("ERROR", "De insert is niet gelukt. " . $error->getMessage());
 			}
+
 
 		} else {
 			// formulier
@@ -72,6 +74,7 @@ switch ($actie) {
 
 			$wijziging = $wijzigen->fetch(PDO::FETCH_ASSOC);
 
+
 			$content->newBlock("FORMULIER");
 			$content->assign('PAGEID', $pagina['idbestanden']);
 			$content->assign('ACTION', 'wijzigen');
@@ -90,6 +93,7 @@ switch ($actie) {
 				$verwijder->bindParam(':id', $_POST['id']);
 
 				$verwijder->execute();
+
 
 				$content->newBlock("SUCCES");
 				$content->assign("SUCCES", "De gebruiker is verwijderd");
@@ -120,6 +124,8 @@ switch ($actie) {
 				$content->assign("ERROR", "De gebruiker is niet gevonden");
 			}
 		}
+
+
 		break;
 	default:
 		try {
