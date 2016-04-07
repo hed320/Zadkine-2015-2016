864a0d8950b80d89955c50a71d3198f5fe14994f
Ronald Nijssen <hed320@hotmail.com>
2016-04-07 10:10:55 +0200

    login

--- Project PHP-2/Lessen/php/login.php
+++ Project PHP-2/Lessen/php/login.php
@@ -7,18 +7,25 @@ if (!empty($_POST["email"]) and !empty($_POST["wachtwoord"])) {
         'cost' => 12,
     ];
     $wachtwoord = password_hash($_POST["wachtwoord"], PASSWORD_BCRYPT, $options);
-    
-    $check = $verbinding->prepare("SELECT count(*) FROM gebruikers WHERE email = :email AND wachtwoord = :wachtwoord");
-    $check->bindParam(":email", $_POST['email']);
-    $check->bindParam(":wachtwoord", $wachtwoord);
-    $check->execute();
 
-    if ($check->fetchColumn() == 1) {
-        $content->newBlock("ERROR");
-        $content->assign("ERROR", "Je login klopt niet");
-    } else {
-        $content->newBlock("SUCCES");
-        $content->assign("SUCCES", "Je bent ingelogd");
+    $checkmail = $verbinding->prepare("SELECT count(*) FROM gebruikers WHERE email = :email");
+    $checkmail->bindParam(":email", $_POST['email']);
+    $checkmail->execute();
+
+    if ($checkmail->fetchColumn() == 1) {
+        $getinfo = $verbinding->prepare("SELECT * FROM gebruikers WHERE email = :email");
+        $getinfo->bindParam(":email", $_POST['email']);
+        $getinfo->execute();
+
+        $info = $getinfo->fetch(PDO::FETCH_ASSOC);
+
+        if (password_verify($_POST["wachtwoord"], $info["wachtwoord"])) {
+            $content->newBlock("SUCCES");
+            $content->assign("SUCCES", "Je bent ingelogd");
+        } else {
+            $content->newBlock("ERROR");
+            $content->assign("ERROR", "Je login klopt niet");
+        }
     }
 } else {
     $content->newBlock("FORMULIER");
